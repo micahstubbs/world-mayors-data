38e862a22fe0aa4e21599ed1214ce201c7d768f8
Micah Stubbs <micah.stubbs@gmail.com>
2018-06-23 00:19:29 -0700

    add page-links

---
+++ scripts/scrape-page-links.js
@@ -0,0 +1,44 @@
+const fs = require('fs')
+const rp = require('request-promise')
+const cheerio = require('cheerio')
+const writeJSON = require('./utils/write-json.js')
+
+// target
+// https://en.wikipedia.org/wiki/Category:Lists_of_mayors
+
+const inputFile = './metadata/subcategory-links.json'
+const subcategoryLinks = JSON.parse(fs.readFileSync(inputFile, 'utf8'))
+
+subcategoryLinks.some((link, i) => {
+  const uriStem = 'https://en.wikipedia.org'
+  const uri = `${uriStem}${link}`
+  let place
+  if (i === 0) place = 'Switzerland'
+  else place = link.split('in_')[1]
+  const transform = body => cheerio.load(body)
+
+  const options = {
+    uri,
+    transform
+  }
+
+  rp(options)
+    .then($ => {
+      const result = $('.mw-category-generated li')
+        .map((i, el) => {
+          return $(el)
+            .find('a')
+            .attr('href')
+        })
+        .get()
+
+      console.log('result', result)
+      writeJSON(result, './metadata/page-links', `${place}-page-links.json`)
+    })
+    .catch(error => {
+      console.error(error)
+    })
+
+  // early stopping for dev mode
+  // if (i > 2) return true
+})
