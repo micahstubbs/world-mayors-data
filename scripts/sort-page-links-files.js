34d914b09d181b698eca0a4e00f051123216a43a
Micah Stubbs <micah.stubbs@gmail.com>
2018-06-23 16:21:11 -0700

    add WIP page sorting script

---
+++ scripts/sort-page-links-files.js
@@ -0,0 +1,13 @@
+const linksAllListPages = require('./links-all-list-pages')
+
+function sortPageLinksFiles(dir) {
+  // read all filenames in directory
+  // filter for only json files
+  // if filename contains links
+  // that all point to list pages
+  //
+  // copy that file over to
+  // bottom-level-page-links
+}
+
+module.exports = sortPageLinksFiles()
