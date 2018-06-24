a9a400d2f14c21ea51dc3424866023f6a1308b78
Micah Stubbs <micah.stubbs@gmail.com>
2018-06-23 18:31:48 -0700

    add more valid patterns to isListPage classifier

--- scripts/is-list-page.js
+++ scripts/is-list-page.js
@@ -1,5 +1,11 @@
 function isListPage(link) {
-  const patterns = ['/wiki/List_of_mayors_', '/wiki/Mayor_of_']
+  const patterns = [
+    '/wiki/List_of_mayors_',
+    '/wiki/Mayor_of_',
+    '/wiki/Mayors_of_',
+    '/wiki/Mayors_in_',
+    '/wiki/Politics_of_'
+  ]
   for (let i = 0; i < patterns.length; i += 1) {
     const pattern = new RegExp(patterns[i].toLowerCase())
     const found = pattern.test(link.toLowerCase())
