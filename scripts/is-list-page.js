d09f79d6902be6441dd703b57733cc8cefebaa1f
Micah Stubbs <micah.stubbs@gmail.com>
2018-06-23 18:24:38 -0700

    make isListPage classifier case-insensitive

--- scripts/is-list-page.js
+++ scripts/is-list-page.js
@@ -1,8 +1,8 @@
 function isListPage(link) {
   const patterns = ['/wiki/List_of_mayors_', '/wiki/Mayor_of_']
   for (let i = 0; i < patterns.length; i += 1) {
-    const pattern = new RegExp(patterns[i])
-    const found = pattern.test(link)
+    const pattern = new RegExp(patterns[i].toLowerCase())
+    const found = pattern.test(link.toLowerCase())
     if (found) return true
   }
   return false
