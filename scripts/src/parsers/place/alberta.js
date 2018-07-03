2678bb0441afebef5c6e1c5dd7be1e7c29a53d8e
Micah Stubbs <micah.stubbs@gmail.com>
2018-07-03 00:36:45 -0700

    add data for red deer alberta

--- scripts/src/parsers/place/alberta.js
+++ scripts/src/parsers/place/alberta.js
@@ -2,6 +2,7 @@ const parseH2TableData = require('../page/h2-table.js')
 const parseH2UlAlphabetList = require('../page/h2-ul-alphabet-list.js')
 const parsePTableNoH2ThumbnailsData = require('../page/p-table-no-h2-thumbnails.js')
 const parsePTableNoH2Data = require('../page/p-table-no-h2.js')
+const parseH2UlData = require('../page/h2-ul.js')
 // const parsePTableData = require('../page/p-table.js')
 // const parsePUlThumbnails = require('../page/p-ul-thumbnails')
 
@@ -15,10 +16,12 @@ function parsePage(props) {
     case '/wiki/List_of_mayors_of_Edmonton':
     case '/wiki/List_of_mayors_of_Fort_Saskatchewan':
     case '/wiki/List_of_mayors_of_Lethbridge':
-    case '/wiki/List_of_mayors_of_Red_Deer,_Alberta':
+
     case '/wiki/List_of_mayors_of_St._Albert,_Alberta':
     case '/wiki/List_of_mayors_of_Strathcona,_Alberta':
       return parsePTableNoH2Data(props)
+    case '/wiki/List_of_mayors_of_Red_Deer,_Alberta':
+      return parseH2UlData(props)
     default:
       return parseH2TableData(props)
   }
--- scripts/src/parsers/row/number-term-name.js
+++ scripts/src/parsers/row/number-term-name.js
@@ -1,3 +1,5 @@
+const parseTerm = require('../field/term.js')
+
 // parse strings like
 // (40) 1904–1911: Pat Lyons
 // 1989-1994: Robert O. Bostwick Jr.
@@ -11,26 +13,45 @@ function parseRow(props) {
     .replace(/\[edit\]/, '')
 
   const rowString = $(el).text()
-  const row = rowString.split(':')
-  const name = row[1].trim()
+
+  // figure out what the row separator character is
+  let row
+  if (/:/.test(rowString)) {
+    row = rowString.split(':')
+  } else if (/\s/.test(rowString)) {
+    row = rowString.split(/\s/)
+    const first = row.shift()
+    const rest = row.join(' ')
+    row = [first, rest]
+  } else {
+    row = [rowString]
+  }
+
+  let name = ''
+  if (row.length > 1) {
+    name = row[1].trim()
+  }
 
   const rowLeft = row[0].split(' ')
-  let number
   let term
+  let number
   // handle two different row formats
   // and two different dash characters
   if (rowLeft.length === 2) {
     number = rowLeft[0]
-    term = rowLeft[1].split('–')
-    if (term.length === 1) term = term[0].split('-')
+    term = parseTerm(rowLeft[1])
+    // term = rowLeft[1].split('–')
+    // if (term.length === 1) term = term[0].split('-')
   } else {
     number = undefined
-    term = rowLeft[0].split('–')
-    if (term.length === 1) term = term[0].split('-')
+    term = parseTerm(rowLeft[0])
+    // term = rowLeft[0].split('–')
+    // if (term.length === 1) term = term[0].split('-')
   }
 
-  const beginTerm = term[0]
-  const endTerm = term[1].replace(/:/, '')
+  // const beginTerm = term[0]
+  // const endTerm = term[1].replace(/:/, '')
+  const { beginTerm, endTerm } = term
 
   const era = parentHeaderText
 
