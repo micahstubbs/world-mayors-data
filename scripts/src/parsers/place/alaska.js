f3caf05bc1c1d404ada4580b6c67ce68ebd9a735
Micah Stubbs <micah.stubbs@gmail.com>
2018-07-02 20:44:00 -0700

    remove unused parser imports from alaska parser

--- scripts/src/parsers/place/alaska.js
+++ scripts/src/parsers/place/alaska.js
@@ -2,11 +2,6 @@ const parseH2TableData = require('../page/h2-table.js')
 const parsePTableData = require('../page/p-table.js')
 const parsePTableNoH2Data = require('../page/p-table-no-h2.js')
 const parsePUlThumbnails = require('../page/p-ul-thumbnails')
-// const parseTableTableData = require('../page/table-table.js')
-// const parseH2UlData = require('../page/h2-ul.js')
-// const parsePUlData = require('../page/p-ul.js')
-// const parseDivUlData = require('../page/div-ul.js')
-// const parseCollapsibleDivUl = require('../page/collapsible-div-ul.js')
 
 function parsePage(props) {
   const { link } = props
