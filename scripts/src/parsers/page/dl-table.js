b5c76dff71b6390e734780b271431556221a2503
micahstubbs <micah.stubbs@gmail.com>
2018-07-04 16:44:43 -0700

    add modern era data for vienna, austria.  first example of using two parsers for one page

---
+++ scripts/src/parsers/page/dl-table.js
@@ -0,0 +1,110 @@
+const cheerio = require('cheerio')
+const cheerioTableparser = require('cheerio-tableparser')
+
+const logger = require('../../utils/logger.js')
+const parseTerm = require('../field/term.js')
+const parseKey = require('../field/key.js')
+
+function parsePage(props) {
+  const { $, category, page } = props
+  // get an array of all sectionHeaders
+  // that are followed by a table
+  const sectionHeadersText = $('h2')
+    .filter((i, el) => {
+      return $(el).next().is('dl')
+    })
+    .map((i, el) => {
+      return $(el).text().replace(/\[edit\]/, '')
+    })
+    .get()
+
+  // get table data
+  // for all tables that are immediately preceded by
+  // an h2 element
+  const tablesData = $('table')
+    .filter((i, el) => {
+      return $(el).prev().is('dl')
+    })
+    .map((i, el) => {
+      cheerioTableparser($)
+      return $(el).parsetable()
+    })
+    .get()
+
+  // get table data by row
+  const tableCount = sectionHeadersText.length
+  const columnsCount = tablesData.length / tableCount
+  const tablesDataByTable = []
+  tablesData.forEach((col, i) => {
+    if (columnsCount % (i + 1) === 0) tablesDataByTable.push([])
+    const tableIndex = Math.floor(i / columnsCount)
+    tablesDataByTable[tableIndex].push(col)
+  })
+
+  const tablesDataByRow = tablesDataByTable.map((table, i) => {
+    return stitchTableData(table, sectionHeadersText[i])
+  })
+
+  function stitchTableData(tableData, era) {
+    if (tableData.length === 0) return tableData
+    const keys = tableData.map(col => col[0])
+    const values = tableData.map(col => col.splice(1, col.length))
+    const rows = []
+
+    const rowKeys = keys.map(k => parseKey(k, page))
+
+    // TODO check that all values are equal length
+    // in other words handle missing values case
+    for (let i = 0; i < values[0].length; i += 1) {
+      const rowObject = {}
+      for (let j = 0; j < rowKeys.length; j += 1) {
+        const currentKey = rowKeys[j]
+        // remove html tags & quotes from values
+        const currentValue = values[j][i]
+          .replace(/<[\w\s=\\"\/\.\?&;\(\)-:%#"]*>/g, '')
+          .replace(/&quot;/g, '')
+        rowObject[currentKey] = currentValue
+
+        if (currentKey === 'years' || currentKey === 'term') {
+          const { beginTerm, endTerm } = parseTerm(currentValue)
+          rowObject.beginTerm = beginTerm
+          rowObject.endTerm = endTerm
+        }
+      }
+      rowObject.era = era
+
+      // if we have parsed out the beginTerm and endTerm
+      // delete the original long string term property
+      if (rowObject.beginTerm && rowObject.endTerm) delete rowObject.term
+      rows.push(rowObject)
+    }
+
+    return rows
+  }
+
+  function parseColumn(col) {
+    const colObject = {}
+    colObject[`col[0]`] = col.slice[(1, col.length)]
+    return colObject
+  }
+
+  logger.info(category)
+  logger.info(page)
+  logger.info('sectionHeadersText', sectionHeadersText)
+  logger.info('')
+
+  console.log(category)
+  console.log(page)
+  console.log('eras', sectionHeadersText)
+  console.log('')
+
+  let allRows = []
+  if (Array.isArray(tablesDataByRow) && tablesDataByRow.length > 0)
+    allRows = tablesDataByRow.reduce((accumulator, currentValue) =>
+      accumulator.concat(currentValue)
+    )
+
+  return allRows
+}
+
+module.exports = parsePage
--- scripts/src/parsers/place/austria.js
+++ scripts/src/parsers/place/austria.js
@@ -1,8 +1,14 @@
 const parseH2UlData = require('../page/h2-ul.js')
+const parseDlTableData = require('../page/dl-table.js')
 
 function parsePage(props) {
   const { link } = props
   switch (link) {
+    case '/wiki/List_of_mayors_of_Vienna':
+      const h2UlData = parseH2UlData(props)
+      const dlTableData = parseDlTableData(props)
+      const combinedData = h2UlData.concat(dlTableData)
+      return combinedData
     default:
       return parseH2UlData(props)
   }
