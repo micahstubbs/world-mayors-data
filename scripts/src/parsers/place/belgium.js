0bd62008614706d78a404d1eed08de2e3a354078
micahstubbs <micah.stubbs@gmail.com>
2018-07-08 23:03:18 -0600

    parse remaining mayors of antwerp.  add h3-table parser

--- scripts/src/parsers/place/belgium.js
+++ scripts/src/parsers/place/belgium.js
@@ -1,6 +1,7 @@
 const parseH2PUlData = require('../page/h2-p-ul.js')
 const parseH2ThumbUl = require('../page/h2-thumb-ul.js')
 const parseH2TableData = require('../page/h2-table.js')
+const parseH3TableData = require('../page/h3-table.js')
 const parseMwHeadlineTableData = require('../page/mw-headline-table.js')
 
 function parsePage(props) {
@@ -15,11 +16,13 @@ function parsePage(props) {
       const mwHeadlineTableData1800s = parseMwHeadlineTableData(props)
       props.headerText = 'United Kingdom of the Netherlands'
       const mwHeadlineTableDataUKN = parseMwHeadlineTableData(props)
+      const h3TableData = parseH3TableData(props)
       const combinedData = h2PUlData
         .concat(h2ThumbUlData)
         .concat(mwHeadlineTableData1700s)
         .concat(mwHeadlineTableData1800s)
         .concat(mwHeadlineTableDataUKN)
+        .concat(h3TableData)
       return combinedData
     default:
       // return parseH2TableData(props)
