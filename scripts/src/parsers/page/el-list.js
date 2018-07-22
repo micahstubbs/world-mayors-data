9dd48c89b4222e42fb869c829e9fd6774c0e4fec
Micah Stubbs <micah.stubbs@gmail.com>
2018-07-22 14:21:35 -0700

    add data for Houston British Columbia.  add generic el-list parser that handles ol ordered lists as well as uls

---
+++ scripts/src/parsers/page/el-list.js
@@ -0,0 +1,96 @@
+const cheerio = require('cheerio')
+
+const getRowParser = require('../get-row-parser.js')
+const logger = require('../../utils/logger.js')
+
+function parsePage(props) {
+  const { $, category, page } = props
+
+  // get list data
+  // for all list elements that are
+  // immediately preceded by
+  // an the element specified in props.el
+  const sectionHeadersTextSet = new Set()
+  let allRows = []
+
+  let listIndex
+  $(props.listEl)
+    .filter((i, el) => {
+      listIndex = i
+      return $(el)
+        .prev()
+        .is(props.el)
+    })
+    .filter((i, el) => {
+      return (
+        $(el)
+          .prev()
+          .text()
+          .replace(/\[edit\]/, '') !== 'See also'
+      )
+    })
+    .filter((i, el) => {
+      return (
+        $(el)
+          .prev()
+          .text()
+          .replace(/\[edit\]/, '') !== 'External links'
+      )
+    })
+    .filter((i, el) => {
+      return (
+        $(el)
+          .parent()
+          .parent()
+          .parent()
+          .attr('id') !== 'mw-navigation'
+      )
+    })
+    .filter((i, el) => {
+      return (
+        $(el)
+          .parent()
+          .parent()
+          .parent()
+          .parent()
+          .attr('id') !== 'mw-navigation'
+      )
+    })
+    .find('li')
+    .each((i, el) => {
+      let rowFormat = 'number-term-name'
+      if (page === 'linz') rowFormat = 'term-name'
+      if (page === 'vienna' || page === 'rio_de_janeiro' || page === 'houston')
+        rowFormat = 'name-term'
+
+      const era = $(el)
+        .parent()
+        .prev()
+        .text()
+        .replace(/\[edit\]/, '')
+      sectionHeadersTextSet.add(era)
+
+      const parsedRows = getRowParser(rowFormat)({
+        $,
+        el,
+        era
+      })
+      allRows = allRows.concat(parsedRows)
+    })
+
+  const sectionHeadersText = Array.from(sectionHeadersTextSet)
+
+  logger.info(category)
+  logger.info(page)
+  logger.info('sectionHeadersText', sectionHeadersText)
+  logger.info('')
+
+  console.log(category)
+  console.log(page)
+  console.log('eras', sectionHeadersText)
+  console.log('')
+
+  return allRows
+}
+
+module.exports = parsePage
--- scripts/src/parsers/place/british_columbia.js
+++ scripts/src/parsers/place/british_columbia.js
@@ -1,5 +1,6 @@
 const h2Table = require('../page/h2-table.js')
 const h2UlAlphabetList = require('../page/h2-ul-alphabet-list.js')
+const elList = require('../page/el-list.js')
 
 function parsePage(props) {
   const { link } = props
@@ -8,6 +9,10 @@ function parsePage(props) {
   switch (link) {
     case '/wiki/List_of_current_mayors_in_British_Columbia':
       return h2UlAlphabetList(props)
+    case '/wiki/List_of_mayors_of_Houston,_British_Columbia':
+      props.el = 'h2'
+      props.listEl = 'ol'
+      return elList(props)
     default:
       return h2Table(props)
   }
