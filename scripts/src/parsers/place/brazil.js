98fae519eac59169a41b87c5eab5865bad2f0a7e
Micah Stubbs <micah.stubbs@gmail.com>
2018-07-22 14:06:29 -0700

    remove commented parsers from brazil

--- scripts/src/parsers/place/brazil.js
+++ scripts/src/parsers/place/brazil.js
@@ -3,11 +3,6 @@ const h2DivTable = require('../page/h2-div-table.js')
 const pTableNoH2 = require('../page/p-table-no-h2.js')
 const elUl = require('../page/el-ul.js')
 
-// const h2PUl = require('../page/h2-p-ul.js')
-// const h2ThumbUl = require('../page/h2-thumb-ul.js')
-// const h3Table = require('../page/h3-table.js')
-// const mwHeadlineTable = require('../page/mw-headline-table.js')
-
 function parsePage(props) {
   const { link } = props
   let combinedData = []
