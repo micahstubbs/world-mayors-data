aa076efda69d380e6bf626d1b2f51f3dad247ee7
Micah Stubbs <micah.stubbs@gmail.com>
2021-01-23 15:13:51 -0800

    add latest progress

--- scripts/src/parsers/place/california.js
+++ scripts/src/parsers/place/california.js
@@ -1,6 +1,7 @@
 const h2Table = require('../page/h2-table.js')
 const h2UlAlphabetList = require('../page/h2-ul-alphabet-list.js')
 const elList = require('../page/el-list.js')
+const pTable = require('../page/p-table.js')
 const pTableNoH2 = require('../page/p-table-no-h2.js')
 const table = require('../page/table.js')
 const h2ThumbTable = require('../page/h2-thumb-table.js')
@@ -10,14 +11,15 @@ function parsePage(props) {
   const { link } = props
   let combinedData = []
   let pTableData
+  let pTableNoh2Data
 
   switch (link) {
     case '/wiki/List_of_mayors_of_Anaheim,_California':
-      pTableData = pTableNoH2(props)
+      pTableNoh2Data = pTableNoH2(props)
       props.el = 'h2'
       props.listEl = 'ul'
       const elListData = elList(props)
-      combinedData = combinedData.concat(pTableData).concat(elListData)
+      combinedData = combinedData.concat(pTableNoh2Data).concat(elListData)
       return combinedData
     case '/wiki/List_of_mayors_of_Berkeley,_California':
       props.el = 'b'
@@ -26,7 +28,7 @@ function parsePage(props) {
     case '/wiki/List_of_mayors_of_Beverly_Hills,_California':
       return thumbTable(props)
     case '/wiki/List_of_mayors_of_Chico,_California':
-      pTableData = pTableNoH2(props)
+      pTableData = pTable(props)
       combinedData = pTableData
       return combinedData
     default:
